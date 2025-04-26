c29dd5fcdd2c6ce6e98f0e3ffaf0c93b58c81d01
mccraigmccraig <mccraigmccraig@gmail.com>
2025-04-26 12:46:12 +0100

    update deps

--- mix.exs
+++ mix.exs
@@ -1,29 +1,34 @@
-defmodule Freer.Mixfile do
+defmodule Freer.MixProject do
   use Mix.Project
 
   def project do
-    [app: :freer,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package,
-     deps: deps]
+    [
+      app: :freer,
+      version: "0.0.1",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      extra_applications: [:logger]
+    ]
   end
 
-  defp package do
-    [files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
-     maintainers: ["Ruoyu Fu"],
-     licenses: ["Apache-2.0"],
-     links: %{"Github" => "https://github.com/lingoer/freer"}]
-  end
+  # defp package do
+  #   [
+  #     files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+  #     maintainers: ["Ruoyu Fu"],
+  #     licenses: ["Apache-2.0"],
+  #     links: %{"Github" => "https://github.com/lingoer/freer"}
+  #   ]
+  # end
+
   # Dependencies can be Hex packages:
   #
   #   {:mydep, "~> 0.3.0"}
@@ -35,8 +40,8 @@ defmodule Freer.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      { :mix_ctags, github: "rgrinberg/mix_ctags" },
-      { :credo, "~> 0.3"}
+      # {:mix_ctags, github: "rgrinberg/mix_ctags"},
+      # {:credo, "~> 0.3"}
     ]
   end
 end
